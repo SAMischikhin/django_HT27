1c207d8d56b5848c8640d7113bdd2a35c01f5137
SAMischikhin <michihin@mail.ru>
2022-02-18 15:53:36 +0300

    fourth

--- utils.py
+++ utils.py
@@ -4,8 +4,8 @@ import os
 from typing import List
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_DIR = BASE_DIR.replace('ads', 'datasets')
-FIXTURE_DIR = BASE_DIR.replace('ads', 'fixtures')
+DATA_DIR = os.path.join(BASE_DIR, 'datasets')
+FIXTURE_DIR = os.path.join(BASE_DIR, 'fixtures')
 
 
 def get_cvs_dict(cvs_data_path: str) -> List[str]:
@@ -14,12 +14,14 @@ def get_cvs_dict(cvs_data_path: str) -> List[str]:
         return [r for r in csv.DictReader(csvfile, field_names)]
 
 
-def get_py_dict(cvslist: List[str]):
+def get_py_dict(cvslist: List[str], name: str):
+    print(cvslist)
+    exit()
     res = []
     for key, item in enumerate(cvslist):
         res.append({
             "pk": key,
-            "model": "ads.category",
+            "model": f"ads.{name}",
             "fields": item
                 })
     return res
@@ -28,7 +30,7 @@ def get_py_dict(cvslist: List[str]):
 def create_fixtures(data_dir: str, fixture_dir: str, name: str):
     cvs_data_path = os.path.join(data_dir, f'{name}.csv')
     cvs_dict = get_cvs_dict(cvs_data_path)
-    data = get_py_dict(cvs_dict)
+    data = get_py_dict(cvs_dict, name)
     json_data_path = os.path.join(fixture_dir, f'{name}.json')
 
     with open(json_data_path, 'w', encoding='UTF-8') as jsonfile:
